cc24ba34ec32d497d2eeb4149cde0ce7141df360
Vladimir HIl <vhi@cabana.dk>
2016-02-20 23:27:49 +0100

    include encodeNameReplacements settings

--- Helpfulcore.Wildcards/ItemResolving/ContentSearchWildcardItemResolver.cs
+++ Helpfulcore.Wildcards/ItemResolving/ContentSearchWildcardItemResolver.cs
@@ -1,17 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Xml;
 using Sitecore;
+using Sitecore.Configuration;
 using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.Linq.Utilities;
 using Sitecore.ContentSearch.SearchTypes;
-using Sitecore.ContentSearch.Security;
 using Sitecore.Data.Items;
 
 namespace Helpfulcore.Wildcards.ItemResolving
 {
     public class ContentSearchWildcardItemResolver : ItemResolver
     {
-        public override Item ResolveItem(Item item, WildcardRouteItem routeItem)
+	    private IDictionary<string, string> encodeNameReplacements;
+	    protected virtual IDictionary<string, string> EncodeNameReplacements
+	    {
+		    get { return this.encodeNameReplacements ?? (this.encodeNameReplacements = this.GetEncodeNameReplacements()); }
+	    }
+
+	    public override Item ResolveItem(Item item, WildcardRouteItem routeItem)
         {
             if (routeItem == null)
             {
@@ -19,36 +29,33 @@ namespace Helpfulcore.Wildcards.ItemResolving
             }
 
             var tokenValues = this.GetTokenValues(item, routeItem);
-
-            var database = "web";
-            if (Context.Database.Name == "master")
-            {
-                database = "master";
-            }
-
+			
 			var rootItem = routeItem.ItemSearchRootNode;
-
-            using (var searchContext = ContentSearchManager.GetIndex("sitecore_" + database + "_index").CreateSearchContext(SearchSecurityOptions.EnableSecurityCheck))
+            
+			using (var searchContext = ContentSearchManager.CreateSearchContext((SitecoreIndexableItem) item))
             {
+				var queryable = searchContext.GetQueryable<SearchResultItem>();
+				
                 var predicate = PredicateBuilder.True<SearchResultItem>();
-                foreach (var itemTemplateId in routeItem.ItemTemplates)
+
+				foreach (var itemTemplateId in routeItem.ItemTemplates)
                 {
                     predicate = predicate.Or(p => p.TemplateId == itemTemplateId.ID);
                 }
 
-                var queryable = searchContext.GetQueryable<SearchResultItem>()
+	            queryable = queryable
 					.Where(predicate)
-                    .Where(resultItem => resultItem.Language == Context.Language.Name);
+					.Where(resultItem => resultItem.Language == Context.Language.Name);
 
-	            if (rootItem != null)
-	            {
+				if (rootItem != null)
+				{
 					queryable = queryable.Where(resultItem => resultItem.Paths.Contains(rootItem.ID));
-	            }
+				}
 
-	            foreach (var tokenRule in tokenValues)
-                {
-                    queryable = this.ConvertRuleToQuery(tokenRule, queryable);
-                }
+				foreach (var tokenRule in tokenValues)
+				{
+					queryable = this.AddTokenPredicates(tokenRule, queryable);
+				}
 
                 var result = queryable.FirstOrDefault();
                 if (result == null)
@@ -66,21 +73,72 @@ namespace Helpfulcore.Wildcards.ItemResolving
             return item;
         }
 
-        protected virtual IQueryable<SearchResultItem> ConvertRuleToQuery(KeyValuePair<string, string> tokenRule, IQueryable<SearchResultItem> queryable)
+		protected IQueryable<SearchResultItem> AddTokenPredicates(KeyValuePair<string, string> tokenRule, IQueryable<SearchResultItem> queryable)
         {
-            return queryable.Where(x =>
-                x[this.GetPropertyIndexName(tokenRule.Key)] == this.GetTokenSearchibleValue(tokenRule.Value) 
-            ||  x[this.GetPropertyIndexName(tokenRule.Key)] == this.GetTokenSearchibleValue(tokenRule.Value).Replace("-", " "));
+			var predicate = PredicateBuilder.True<SearchResultItem>();
+			var indexFieldName = this.GetPropertyIndexName(tokenRule.Key);
+	        var searchTerm = this.GetTokenSearchibleValue(tokenRule.Value);
+
+			foreach (var replacement in this.EncodeNameReplacements)
+	        {
+		        var value = replacement.Value;
+		        if (searchTerm.ToLower().Contains(value.ToLower()))
+		        {
+					foreach (var name in EncodeNameReplacements.Where(x => x.Value == value))
+					{
+						var searchTermVariant = searchTerm.Replace(name.Value, name.Key);
+						predicate = predicate.Or(p => p[indexFieldName] == searchTermVariant);
+					}
+		        }
+	        }
+
+			predicate = predicate.Or(p => p[indexFieldName] == searchTerm);
+
+	        return queryable.Where(predicate);
         }
 
         protected virtual string GetTokenSearchibleValue(string value)
         {
-            return "%22" + value + "*%22";
+            return "%22" + value + "%22";
         }
 
         protected virtual string GetPropertyIndexName(string key)
         {
             return key.ToLower().Replace(" ", "_").Replace("-", "_");
         }
+
+	    protected virtual IDictionary<string, string> GetEncodeNameReplacements()
+	    {
+			var ret = new Dictionary<string, string>();
+
+			var nameReplacements = Factory.GetConfigNode("encodeNameReplacements", false);
+
+			if (nameReplacements != null)
+		    {
+				foreach (XmlNode replacementNode in nameReplacements.ChildNodes)
+			    {
+				    if (replacementNode.Attributes != null)
+				    {
+					    var modeAttr = replacementNode.Attributes["mode"];
+						var findAttr = replacementNode.Attributes["find"];
+						var replaceWithAttr = replacementNode.Attributes["replaceWith"];
+						if (modeAttr != null && findAttr != null && replaceWithAttr != null)
+					    {
+						    var mode = modeAttr.Value;
+							var find = findAttr.Value;
+							var replaceWith = replaceWithAttr.Value;
+
+							if ("on".Equals(mode, StringComparison.InvariantCultureIgnoreCase) && !ret.ContainsKey(find) && !string.IsNullOrEmpty(find))
+						    {
+								ret.Add(find, replaceWith);
+						    }
+					    }
+				    }
+				    
+			    }
+		    }
+
+		    return ret;
+	    }
     }
 }
